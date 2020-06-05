f0d72728fc3cd09a854753b622c1594c5fce63c7
Samantha Wabi <swfullpotential@gmail.com>
2020-06-05 21:52:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,5 +109,19 @@ class TicTacToe
     end
   end
   
+  def winner
+    index = []
+    index = won? 
+    if index == false 
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else 
+        return "O"
+      end
+    end
+  end
+  
   
 end
