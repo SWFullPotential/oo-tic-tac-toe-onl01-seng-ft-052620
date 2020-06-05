d0314462cf34da828053839c7c62909d509cedc3
Samantha Wabi <swfullpotential@gmail.com>
2020-06-05 21:06:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,5 +47,14 @@ class TicTacToe
     return turn
   end
   
+  def current_player
+    num_turns = turn_count
+    if num_turns % 2 == 0 
+      player = "X"
+    else
+      player = "O"
+    end
+    return player
+  end
   
 end
